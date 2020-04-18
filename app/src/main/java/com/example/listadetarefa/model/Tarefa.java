1c6f2602ddfde68b06836750b56fcbbead34235f
Jack Pecini <jacksonpecini10@gmail.com>
2020-04-17 22:52:24 -0300

    criado lista

---
+++ app/src/main/java/com/example/listadetarefa/model/Tarefa.java
@@ -0,0 +1,18 @@
+package com.example.listadetarefa.model;
+
+public class Tarefa {
+
+    public String title;
+
+    public Tarefa(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
