c8aba6031d545dcf02e51468964b58fe50812762
Jack Pecini <jacksonpecini10@gmail.com>
2020-04-17 22:07:31 -0300

    Initial commit

---
+++ app/src/main/java/com/example/listadetarefa/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.listadetarefa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
